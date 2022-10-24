299a4e97b6b10e6585f397786c4c753953049997
irishmaiden18 <laurapohl18@gmail.com>
2022-10-24 10:54:54 -0400

    remove old files

--- public/tuiter/NavigationSidebar/script.js
+++
@@ -1,8 +0,0 @@
-import NavigationSidebar from "./index.js";
-
-$('#container').append(`
-   <div>
-       <h1>Navigation</h1>
-       ${NavigationSidebar()}
-   </div>
-`);
--- public/tuiter/homeScreen/PostList/HomeComponent.js
+++
@@ -1,6 +0,0 @@
-import postItem from "./postItem.js";
-import PostList from "./PostList";
-
-
-PostList
-$(postItem);
