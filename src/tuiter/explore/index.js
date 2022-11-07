fa3e351d3bf9642fbaf44961521621e3e54e8ab6
irishmaiden18 <laurapohl18@gmail.com>
2022-11-06 23:45:31 -0500

    fixing netlify errors part 2

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import PostSummaryList from "../post-summary-list";
 import "./index.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Link} from "react-router-dom";
 import Bibliography from "../bibliography";
 
 const ExploreComponent = () => {
